c0f801e4e5e73908e9c71b4114934cd93a52f498
tomax <1121584497@qq.com>
2019-07-13 18:50:53 +0800

    update redis command handler

--- harcs/harcs-proxy/src/main/java/org/nuaa/tomax/harcs/handler/RedisCommandHandler.java
+++ harcs/harcs-proxy/src/main/java/org/nuaa/tomax/harcs/handler/RedisCommandHandler.java
@@ -1,13 +1,12 @@
 package org.nuaa.tomax.harcs.handler;
 
-import org.nuaa.tomax.harcs.handler.bean.RedisMessageHandleContext;
-import org.nuaa.tomax.harcs.handler.msg.RedisMessageHandler;
+import org.nuaa.tomax.harcs.bean.RedisMessageHandleContext;
+import org.nuaa.tomax.harcs.common.RedisMessageUtil;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.redis.*;
-
-import java.util.concurrent.ConcurrentHashMap;
+import org.nuaa.tomax.harcs.handler.msg.RedisMessageHandler;
 
 /**
  * @Name: RedisCommandHandler
@@ -17,43 +16,29 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Version: 1.0
  */
 public class RedisCommandHandler extends ChannelInboundHandlerAdapter {
-    public static ConcurrentHashMap<String, ChannelInfo> channelMessages = new ConcurrentHashMap<String, ChannelInfo>();
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         RedisMessage redisMessage = (RedisMessage) msg;
 
-        RedisMessageHandler.dispatchHandle(redisMessage, new RedisMessageHandleContext());
+        RedisMessageUtil.printRedisMessage(redisMessage);
+        RedisMessage reply = RedisMessageHandler.dispatchHandle(redisMessage, new RedisMessageHandleContext());
+
+        ctx.writeAndFlush(reply);
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        String id = ctx.channel().id().asShortText();
-        ChannelInfo info = new ChannelInfo();
-//        if ("".equals(Configuration.HAREDIS_ACCESS_PASSWORD)) {
-            info.setAuth(true);
-//        }
-        channelMessages.put(id, info);
+
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        String id = ctx.channel().id().asShortText();
-        channelMessages.remove(id);
-        Channel channel = ctx.channel();
-        if (channel.isActive()) {
-            ctx.close();
-        }
+
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        cause.printStackTrace();
-        String id = ctx.channel().id().asShortText();
-        channelMessages.remove(id);
-        Channel channel = ctx.channel();
-        if (channel.isActive()) {
-            ctx.close();
-        }
+
     }
 }
