72cafb1a2a09ade163d963dd7c1f1c68d9d79536
tomax <1121584497@qq.com>
2019-07-13 20:34:57 +0800

    add log factory

---
+++ harcs/harcs-proxy/src/main/java/org/nuaa/tomax/harcs/common/LogFactory.java
@@ -0,0 +1,17 @@
+package org.nuaa.tomax.harcs.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Name: LogFactory
+ * @Description: TODO
+ * @Author: tomax
+ * @Date: 2019-07-13 20:19
+ * @Version: 1.0
+ */
+public class LogFactory {
+    public static Logger getLog() {
+        return LoggerFactory.getLogger(LogFactory.class);
+    }
+}
