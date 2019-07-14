e86ecbf9ce18b28687f235ad285d3c58d48b9c32
tomax <1121584497@qq.com>
2019-07-14 14:56:31 +0800

    add dispatch handler

---
+++ harcs/harcs-proxy/src/main/java/org/nuaa/tomax/harcs/handler/DispatchHandler.java
@@ -0,0 +1,12 @@
+package org.nuaa.tomax.harcs.handler;
+
+/**
+ * @Name: DispatchHandler
+ * @Description: TODO
+ * @Author: tomax
+ * @Date: 2019-07-14 14:43
+ * @Version: 1.0
+ */
+public class DispatchHandler {
+
+}
