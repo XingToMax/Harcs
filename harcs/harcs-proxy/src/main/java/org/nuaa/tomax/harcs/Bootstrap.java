fbdc2088bef875a01b638a3bb3877791b417a434
tomax <1121584497@qq.com>
2019-07-14 14:59:48 +0800

    add proxy server bootstrap

---
+++ harcs/harcs-proxy/src/main/java/org/nuaa/tomax/harcs/Bootstrap.java
@@ -0,0 +1,16 @@
+package org.nuaa.tomax.harcs;
+
+import org.nuaa.tomax.harcs.client.MonitorClient;
+
+/**
+ * @Name: Bootstrap
+ * @Description: TODO
+ * @Author: tomax
+ * @Date: 2019-07-14 14:29
+ * @Version: 1.0
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+        MonitorClient.info(8087);
+    }
+}
